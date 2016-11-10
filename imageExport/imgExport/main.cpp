72c43f582bb0db104979a103199644ccd4a21382
Jisu <redgem92@gmail.com>
2016-11-10 14:24:42 +0900

    imageExport runtime error fixed

--- imageExport/imgExport/main.cpp
+++ imageExport/imgExport/main.cpp
@@ -21,7 +21,7 @@ String eyes_cascade_name = "haarcascade_eye_tree_eyeglasses.xml";
 CascadeClassifier face_cascade;
 CascadeClassifier eyes_cascade;
 
-vector<Rect> face_pos; //얼굴 벡터값 저장
+vector<Rect>* face_pos; //얼굴 벡터값 저장
 
 
 
@@ -76,21 +76,21 @@ int main(int argc, const char** argv)
 			cvtColor(img, gray, CV_RGB2GRAY);
 
 			//사각 출력 그래픽
-
-			face_cascade.detectMultiScale(gray, face_pos, 1.1, 3, 0 | CV_HAAR_SCALE_IMAGE,
+			face_pos = new vector<Rect>;
+			face_cascade.detectMultiScale(gray, (*face_pos), 1.1, 3, 0 | CV_HAAR_SCALE_IMAGE,
 				Size(10, 10));
 
-			/*for (int k = 0; k < (int)face_pos.size(); k++) {
-			rectangle(img, face_pos[k], Scalar(0, 255, 0), 2);
+			/*for (int k = 0; k < (int)(*face_pos).size(); k++) {
+			rectangle(img, (*face_pos)[k], Scalar(0, 255, 0), 2);
 			}*/
 
 #if 0
 			//자른 얼굴 시작점 추출
-			newImage = cv::Mat::zeros(cv::Size(face_pos[0].width, face_pos[0].height), img.type());
+			newImage = cv::Mat::zeros(cv::Size((*face_pos)[0].width, (*face_pos)[0].height), img.type());
 
-			for (int i = 0; i < face_pos[0].width; i++) {
-				for (int j = 0; j < face_pos[0].height; j++) {
-					newImage.at<Vec3b>(j, i) = img.at<Vec3b>(face_pos[0].y + j, face_pos[0].x + i);
+			for (int i = 0; i < (*face_pos)[0].width; i++) {
+				for (int j = 0; j < (*face_pos)[0].height; j++) {
+					newImage.at<Vec3b>(j, i) = img.at<Vec3b>(face_pos[0].y + j, (*face_pos)[0].x + i);
 				}
 			}
 			
@@ -99,7 +99,7 @@ int main(int argc, const char** argv)
 			/* --- Cropping facial area with Rect --- */
 #if 0
 			// Set rectangle position x, y and width, height to cropping
-			Rect rect(face_pos[0].x, face_pos[0].y, face_pos[0].width, face_pos[0].height); 
+			Rect rect((*face_pos)[0].x, (*face_pos)[0].y, (*face_pos)[0].width, (*face_pos)[0].height); 
 
 			// Cropping rectangle facial area with rect()
 			newImage = img(rect); 
@@ -107,14 +107,14 @@ int main(int argc, const char** argv)
 
 			/* --- Circular face extraction --- */
 #if 1
-			newImage = cv::Mat::zeros(cv::Size(face_pos[0].width, face_pos[0].height), img.type());
+			newImage = cv::Mat::zeros(cv::Size((*face_pos)[0].width, (*face_pos)[0].height), img.type());
 
-			int face_radius = (int)(face_pos[0].width / 2); // Radius of circular facial area
+			int face_radius = (int)((*face_pos)[0].width / 2); // Radius of circular facial area
 			int d;											// Length between middle of the face & at<Vec3b>(j, i)
 
-			for (int i = 0; i < face_pos[0].width; i++) {
-				for (int j = 0; j < face_pos[0].height; j++) {
-					d = sqrt(pow((int)(face_pos[0].width / 2) - i, 2) + pow((int)(face_pos[0].height / 2) - j, 2));
+			for (int i = 0; i < (*face_pos)[0].width; i++) {
+				for (int j = 0; j < (*face_pos)[0].height; j++) {
+					d = sqrt(pow((int)((*face_pos)[0].width / 2) - i, 2) + pow((int)((*face_pos)[0].height / 2) - j, 2));
 
 					// Check each point
 					if(d > face_radius)	{
@@ -123,7 +123,7 @@ int main(int argc, const char** argv)
 					}
 					else {
 						// Point is included in circle
-						newImage.at<Vec3b>(j, i) = img.at<Vec3b>(face_pos[0].y + j, face_pos[0].x + i);
+						newImage.at<Vec3b>(j, i) = img.at<Vec3b>((*face_pos)[0].y + j, (*face_pos)[0].x + i);
 					}
 				}
 			}
@@ -143,13 +143,13 @@ int main(int argc, const char** argv)
 
 	////원형(타원) 출력 그래픽 -- 미구현
 	///*
-	//for (size_t i = 0; i < face_pos.size(); i++) {
-	//	Point center(face_pos[i].x + face_pos[i].width*0.5, face_pos[i].y +
-	//		face_pos[i].height*0.5);
-	//	ellipse(img, center, Size(face_pos[i].width*0.5, face_pos[i].height*0.5), 0,
+	//for (size_t i = 0; i < (*face_pos).size(); i++) {
+	//	Point center((*face_pos)[i].x + (*face_pos)[i].width*0.5, (*face_pos)[i].y +
+	//		(*face_pos)[i].height*0.5);
+	//	ellipse(img, center, Size((*face_pos)[i].width*0.5, (*face_pos)[i].height*0.5), 0,
 	//		0, 360, Scalar(255, 0, 255), 4, 8, 0);
 
-	//	Mat faceROI = gray(face_pos[i]);
+	//	Mat faceROI = gray((*face_pos)[i]);
 	//	vector<Rect> eyes;
 
 	//	//각각 얼굴 안구 인식
@@ -158,7 +158,7 @@ int main(int argc, const char** argv)
 
 	//	for (size_t j = 0; j < eyes.size(); j++)
 	//	{
-	//		Point center(face_pos[i].x + eyes[j].x + eyes[j].width*0.5, face_pos[i].y
+	//		Point center((*face_pos)[i].x + eyes[j].x + eyes[j].width*0.5, (*face_pos)[i].y
 	//			+ eyes[j].y + eyes[j].height*0.5);
 	//		int radius = cvRound((eyes[j].width + eyes[j].height)*0.25);
 	//		circle(frame, center, radius, Scalar(255, 0, 0), 4, 8, 0);
@@ -166,7 +166,7 @@ int main(int argc, const char** argv)
 	//}
 	//*/
 
-
+	face_pos->clear();
 
 	return 0;
 
