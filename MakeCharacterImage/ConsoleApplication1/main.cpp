96b549c73f7f01deb5df3e951753c30b71864cab
MunChanSeong <ckan0822@naver.com>
2016-11-19 19:26:30 +0900

    원 검출을 이용한 캐릭터 이미지 만들기

--- MakeCharacterImage/ConsoleApplication1/main.cpp
+++ MakeCharacterImage/ConsoleApplication1/main.cpp
@@ -51,10 +51,20 @@ void on_mouse(int event, int x, int y, int flag, void*params) {
 
 int main(void) {
 
-	cv::namedWindow("image");
+	//cv::namedWindow("image");
 	img2 = cv::imread("Character\\turtle.png");
 
-	cv::setMouseCallback("image", on_mouse);
+	cv::Mat gray;
+
+	cv::cvtColor(img2, gray, cv::COLOR_BGR2GRAY);
+
+	std::vector<cv::Vec3f> circles;
+
+	GaussianBlur(gray, gray, cv::Size(7, 7), 3, 3);
+
+	cv::HoughCircles(gray, circles, cv::HOUGH_GRADIENT, 1, 10);
+
+	/*cv::setMouseCallback("image", on_mouse);
 
 	cv::imshow("image", img2);
 
@@ -62,7 +72,7 @@ int main(void) {
 		char ch = cv::waitKey(0);
 		if (ch == 27)
 			break;
-	}
+	}*/
 
 	DIR *dir = opendir("\images");
 	struct dirent *ent;
@@ -82,24 +92,34 @@ int main(void) {
 
 			img = cv::imread(name);
 
-			cv::resize(img, img, cv::Size(width, hei), 0, 0, CV_INTER_NN);
+			for (size_t e = 0; e < circles.size(); e++) {
 
-			//cv::imshow("aa", img);
+				int face_radius = cvRound(circles[e][2]); // Radius of circular facial area
 
-			int m = 0;
-			int k = 0;
+				lupoint_x = cvRound(circles[e][0]) - face_radius;
+				lupoint_y = cvRound(circles[e][1]) - face_radius;
+				rdpoint_x = lupoint_x + 2 * face_radius;
+				rdpoint_y = lupoint_y + 2 * face_radius;
 
-			int face_radius = (int)(width / 2); // Radius of circular facial area
-			int d;
+				cv::resize(img, img, cv::Size(2 * face_radius, 2 * face_radius), 0, 0, CV_INTER_NN);
 
-			for (int i = lupoint_x; i < rdpoint_x; i++, m++) {
-				k = 0;
-				for (int j = lupoint_y; j < rdpoint_y; j++, k++) {
-					d = sqrt(pow((int)(width / 2) - (i - lupoint_x), 2) + pow((int)(hei / 2) - (j - lupoint_y), 2));
-					if (d < face_radius) {
-						gMatChangeImage.at<cv::Vec3b>(j, i)[0] = img2.at<cv::Vec3b>(j, i)[0] * (1.0f - (face_radius - d) * 0.01f) + img.at<cv::Vec3b>(k, m)[0] * (face_radius - d) * 0.01f;
-						gMatChangeImage.at<cv::Vec3b>(j, i)[1] = img2.at<cv::Vec3b>(j, i)[1] * (1.0f - (face_radius - d) * 0.01f) + img.at<cv::Vec3b>(k, m)[1] * (face_radius - d) * 0.01f;
-						gMatChangeImage.at<cv::Vec3b>(j, i)[2] = img2.at<cv::Vec3b>(j, i)[2] * (1.0f - (face_radius - d) * 0.01f) + img.at<cv::Vec3b>(k, m)[2] * (face_radius - d) * 0.01f;
+				//cv::imshow("aa", img);
+
+				int m = 0;
+				int k = 0;
+
+				
+				int d;
+
+				for (int i = lupoint_x; i < rdpoint_x; i++, m++) {
+					k = 0;
+					for (int j = lupoint_y; j < rdpoint_y; j++, k++) {
+						d = sqrt(pow((int)(face_radius) - (i - lupoint_x), 2) + pow((int)(face_radius / 2) - (j - lupoint_y), 2));
+						if (d < face_radius) {
+							gMatChangeImage.at<cv::Vec3b>(j, i)[0] = img2.at<cv::Vec3b>(j, i)[0] * (1.0f - (face_radius - d) * 0.01f) + img.at<cv::Vec3b>(k, m)[0] * (face_radius - d) * 0.01f;
+							gMatChangeImage.at<cv::Vec3b>(j, i)[1] = img2.at<cv::Vec3b>(j, i)[1] * (1.0f - (face_radius - d) * 0.01f) + img.at<cv::Vec3b>(k, m)[1] * (face_radius - d) * 0.01f;
+							gMatChangeImage.at<cv::Vec3b>(j, i)[2] = img2.at<cv::Vec3b>(j, i)[2] * (1.0f - (face_radius - d) * 0.01f) + img.at<cv::Vec3b>(k, m)[2] * (face_radius - d) * 0.01f;
+						}
 					}
 				}
 			}
