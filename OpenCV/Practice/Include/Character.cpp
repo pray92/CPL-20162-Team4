08b9b7222b45ba9d2bb6e118e4f2c3266beae3ad
CastleSpike <hongdh1106@gmail.com>
2016-11-20 12:24:23 +0900

    Turtle Movement added
    Turtle random move

--- OpenCV/Practice/Include/Character.cpp
+++ OpenCV/Practice/Include/Character.cpp
@@ -20,7 +20,14 @@ HRESULT CCharacter::Initialize(void)
 	}
 
 	m_vPos = D3DXVECTOR3(rand() % WINSIZEX, rand() % WINSIZEY, 0.f);
-	m_vCenter = D3DXVECTOR3( 142.f, 142.f, 0.f);
+	m_vDestination = D3DXVECTOR3(rand() % WINSIZEX, rand() % WINSIZEY, 0.f);	// Set destination
+	m_vDirection = GetUnitVec(m_vPos, m_vDestination);							// Unit vector of the m_vPos
+	m_vCenter = D3DXVECTOR3(142.f, 142.f, 0.f);
+
+	m_vStartPos = m_vPos;
+	movingSpeed = 0;
+	maxSpeed = (rand() % 20) / 10 + 0.5;
+	acceleration = maxSpeed / 250;
 
 	m_iFrame = 0;
 	m_dwTime = GetTickCount();
@@ -30,7 +37,42 @@ HRESULT CCharacter::Initialize(void)
 
 SCENE CCharacter::Progress(void)
 {
+	if (GetDMagnitude(m_vStartPos, m_vPos) < (GetDMagnitude(m_vStartPos, m_vDestination) / 3))
+	{
+		// Accelerate
+		movingSpeed += acceleration;
+	}
+	else if (GetDMagnitude(m_vStartPos, m_vPos) < (GetDMagnitude(m_vStartPos, m_vDestination) * 2 / 3))
+	{
+		//
+		movingSpeed = maxSpeed;
+	}
+	else
+	{
+		// Deaccelerate
+		if ((movingSpeed -= acceleration) < 0.1)
+			movingSpeed = 0.1;
+	}
+
+	// Move character position to the destination
+	m_vPos.x += m_vDirection.x * movingSpeed;
+	m_vPos.y += m_vDirection.y * movingSpeed;
+	m_vPos.z += m_vDirection.z * movingSpeed;
+
+	// Set next position
+	if (GetDMagnitude(m_vPos, m_vDestination) < 10)
+	{
+		m_vStartPos = m_vDestination;
+		m_vDestination = D3DXVECTOR3(rand() % WINSIZEX, rand() % WINSIZEY, 0.f);
+
+		m_vDirection = GetUnitVec(m_vPos, m_vDestination);
+
+		movingSpeed = 0;
+		maxSpeed = (rand() % 20) / 10 + 0.5;
+	}
+
 	Frame();
+
 	return SCENE_NONPASS;
 }
 
@@ -46,7 +88,16 @@ void CCharacter::Render(void)
 	//크기
 	D3DXMATRIX matScale;
 	D3DXMatrixIdentity(&matScale);
-	D3DXMatrixScaling(&matScale, 1.f, 1.f, 1.f);
+
+	if (m_vDestination.x - m_vPos.x < 0)
+	{
+		D3DXMatrixScaling(&matScale, -1.f, 1.f, 1.f);
+	}
+	else
+	{
+		D3DXMatrixScaling(&matScale, 1.f, 1.f, 1.f);
+	}
+
 	//위치
 	D3DXMATRIX matTrans;
 	D3DXMatrixIdentity(&matTrans);
@@ -68,15 +119,38 @@ RECT CCharacter::Frame()
 	RECT rcFrame;
 	if (GetTickCount() > m_dwTime + 500)
 	{
-		++m_iFrame; 
+		++m_iFrame;
 		m_dwTime = GetTickCount();
 	}
 	if (m_iFrame > 4)
 		m_iFrame = 0;
 
-	rcFrame = { 283 * m_iFrame, 0, 283 * m_iFrame + 283, 283};
+	rcFrame = { 283 * m_iFrame, 0, 283 * m_iFrame + 283, 283 };
 
 	return rcFrame;
+}
+
+D3DXVECTOR3 CCharacter::GetUnitVec(const D3DXVECTOR3 m_vPos1, const D3DXVECTOR3 m_vPos2)
+{
+	int vectorLength;
+	D3DXVECTOR3 m_vUnit = m_vPos2 - m_vPos1;
+
+	vectorLength = sqrt(pow(m_vUnit.x, 2) + pow(m_vUnit.y, 2) + pow(m_vUnit.z, 2));
+
+	m_vUnit.x = m_vUnit.x / vectorLength;
+	m_vUnit.y = m_vUnit.y / vectorLength;
+	m_vUnit.z = m_vUnit.z / vectorLength;
+
+	return  m_vUnit;
+}
+
+float CCharacter::GetDMagnitude(const D3DXVECTOR3 m_vPos1, const D3DXVECTOR3 m_vPos2)
+{
+	float length;
+
+	length = pow(m_vPos2.x - m_vPos1.x, 2) + pow(m_vPos2.y - m_vPos1.y, 2) + pow(m_vPos2.z - m_vPos1.z, 2);
+
+	return length;
 }
 
 CCharacter::CCharacter()
--- OpenCV/Practice/Include/Character.h
+++ OpenCV/Practice/Include/Character.h
@@ -6,7 +6,14 @@ private:
 	static bool m_bTextCalled;
 private:
 	D3DXVECTOR3		m_vPos;
+	D3DXVECTOR3		m_vStartPos;
+	D3DXVECTOR3		m_vDestination;
 	D3DXVECTOR3		m_vCenter;
+
+	D3DXVECTOR3		m_vDirection;
+	float			movingSpeed;
+	float			maxSpeed;
+	float			acceleration;
 private:
 	int				m_iFrame;
 	DWORD			m_dwTime;
@@ -19,8 +26,9 @@ public:
 	D3DXVECTOR3 GetPos() const { return m_vPos; };
 private:
 	RECT Frame();
+	D3DXVECTOR3 GetUnitVec(const D3DXVECTOR3, const D3DXVECTOR3);
+	float GetDMagnitude(const D3DXVECTOR3, const D3DXVECTOR3);		// Return length square between two points
 public:
 	CCharacter();
 	~CCharacter();
 };
-
